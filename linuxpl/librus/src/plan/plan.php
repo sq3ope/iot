7aa9fbfdfb417616ea64d81f9e4ae8765e1cebba
Pawel Firlej <pawel.firlej@payu.pl>
2019-10-01 23:17:20 +0200

    Librus plan template

---
+++ linuxpl/librus/src/plan/plan.php
@@ -0,0 +1,3 @@
+login: <?php print $config['librus_login']; ?>
+<br/>
+password: <?php print $config['librus_password']; ?>
---
+++ linuxpl/librus/src/plan/tomek.php
@@ -0,0 +1,4 @@
+<?php
+	include_once('../config/tomek.inc.php');
+	include_once('plan.php');
+?>
