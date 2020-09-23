bc894586ddf8b7c90b44d869e62a67af00892be4
Pawel Firlej <pawel.firlej@payu.pl>
2020-09-23 22:43:15 +0200

    Hide sensitive data

--- linuxpl/librus/deploy_to_remote_host.sh
+++ linuxpl/librus/deploy_to_remote_host.sh
@@ -1,6 +1,5 @@
-username=zulu
-host=zulu.vdl.pl
-port=59184
+project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source "$project_dir/src/config/remote.inc"
 
 rsync -av -e "ssh -p 59184" src/ "$username@$host:public_html/librus/"
 ssh -p $port "$username@$host" "./public_html/librus/create_dummy_files.sh ./public_html"
