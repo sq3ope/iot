db42d11e8128a3f00c5d8cda45c95f88beaf50f5
Pawel Firlej <pawel.firlej@payu.pl>
2022-12-18 20:23:05 +0100

    Take ssh port from config

--- linuxpl/librus/deploy_to_remote_host.sh
+++ linuxpl/librus/deploy_to_remote_host.sh
@@ -1,5 +1,5 @@
 project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$project_dir/src/config/remote.inc"
 
-rsync -av -e "ssh -p 59184" src/ "$username@$host:public_html/librus/"
+rsync -av -e "ssh -p $port" src/ "$username@$host:public_html/librus/"
 ssh -p $port "$username@$host" "./public_html/librus/create_dummy_files.sh ./public_html"
