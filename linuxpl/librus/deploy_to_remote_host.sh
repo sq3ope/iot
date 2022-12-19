04cbea26f5f46a0d19915013463660e869650127
Pawel Firlej <sq3ope@gmail.com>
2022-12-19 22:12:53 +0100

    Handle file permissions on Google Drive

--- linuxpl/librus/deploy_to_remote_host.sh
+++ linuxpl/librus/deploy_to_remote_host.sh
@@ -2,4 +2,10 @@ project_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$project_dir/src/config/remote.inc"
 
 rsync -av -e "ssh -p $port" src/ "$username@$host:public_html/librus/"
-ssh -p $port "$username@$host" "./public_html/librus/create_dummy_files.sh ./public_html"
+ssh -p $port "$username@$host" <<EOF
+./public_html/librus/create_dummy_files.sh ./public_html
+chmod -R o+r ~/public_html/librus
+find  ~/public_html/librus -type d -exec chmod 755 {} \;
+find ~/public_html/librus -type f -exec chmod 644 {} \;
+EOF
+
